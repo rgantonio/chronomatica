e4846acd78c204b97dd717c72450fc4801d65b41
rgantonio <ryan.antonio@esat.kuleuven.be>
2025-09-05 11:35:30 +0200

    lib: fmt

--- lib/vsa.py
+++ lib/vsa.py
@@ -1,11 +1,34 @@
-'''
-  Python library for Vector Symbolic Architectures (VSA)
-'''
+"""
+Python library for Vector Symbolic Architectures (VSA)
+"""
 
 import numpy as np
 
+
+# Binary/bipolar random indexed method for generating hypervectors
+# This works for binary and bipolar hypervectors only
+def gen_rand_idx_hv(size, type="bipolar"):
+    """
+    Generate a random indexed hypervector of a specified type.
+    Parameters:
+    size (int): The size of the hypervector.
+    type (str): The type of hypervector. Can be 'bipolar' or 'binary'.
+    Returns:
+    np.ndarray: The generated hypervector.
+    """
+    hv = np.arange(size)
+    np.random.shuffle(hv)
+    if type == "binary":
+        return_hv = (hv >= size // 2).astype(int)
+    elif type == "bipolar":
+        return_hv = np.where(hv >= size // 2, 1, -1)
+    else:
+        raise ValueError("Unsupported hypervector type")
+    return return_hv
+
+
 # Generate hypervector of bipolar values
-def gen_hv(size, type='bipolar'):
+def gen_hv(size, type="bipolar", int_min=0, int_max=255):
     """
     Generate a hypervector of a specified type.
     Parameters:
@@ -14,18 +37,23 @@ def gen_hv(size, type='bipolar'):
     Returns:
     np.ndarray: The generated hypervector.
     """
-    if type == 'bipolar':
-        return np.random.choice([-1, 1], size=size)
-    elif type == 'binary':
-        return np.random.choice([0, 1], size=size)
-    elif type == 'real':
+    if type == "bipolar":
+        return gen_rand_idx_hv(size, type="bipolar")
+    elif type == "binary":
+        return gen_rand_idx_hv(size, type="binary")
+    elif type == "integer":
+        return np.random.randint(int_min, int_max, size=size)
+    elif type == "real":
         return np.random.uniform(-1, 1, size=size)
-    elif type == 'complex':
-        return np.random.uniform(-1, 1, size=size) + 1j * np.random.uniform(-1, 1, size=size)
+    elif type == "complex":
+        return np.random.uniform(-1, 1, size=size) + 1j * np.random.uniform(
+            -1, 1, size=size
+        )
     else:
         raise ValueError("Unsupported hypervector type")
-  
-  # Dot product of two hypervectors
+
+
+# Dot product of two hypervectors
 def hv_dot(hv1, hv2):
     """
     Compute the dot product of two hypervectors.
@@ -36,6 +64,7 @@ def hv_dot(hv1, hv2):
     float: The dot product of the two hypervectors.
     """
     return np.dot(hv1, hv2)
+
 
 # Cosine similarity between two hypervectors
 def hv_cos(hv1, hv2):
@@ -50,12 +79,13 @@ def hv_cos(hv1, hv2):
     dot_product = hv_dot(hv1, hv2)
     norm_hv1 = np.linalg.norm(hv1)
     norm_hv2 = np.linalg.norm(hv2)
-    
+
     if norm_hv1 == 0 or norm_hv2 == 0:
         return 0.0
     else:
         return dot_product / (norm_hv1 * norm_hv2)
-    
+
+
 # Hamming distance between two hypervectors
 def hv_ham(hv1, hv2, normalize=True):
     """
@@ -71,6 +101,7 @@ def hv_ham(hv1, hv2, normalize=True):
     else:
         return np.sum(hv1 != hv2)
 
+
 # Circular permutation of a hypervector
 def hv_perm(hv, shift):
     """
@@ -82,6 +113,7 @@ def hv_perm(hv, shift):
     np.ndarray: The permuted hypervector.
     """
     return np.roll(hv, shift)  # Circular shift
+
 
 # Adding hypervectors in a list with
 # an sign magnitude to convert to bipolar
@@ -97,14 +129,15 @@ def hv_add(hvs, sign_magnitude=False, threshold=None):
     np.ndarray: The resulting hypervector after addition.
     """
     result = np.sum(hvs, axis=0)
-    
+
     if sign_magnitude:
         result = np.sign(result)
-    
+
     if threshold is not None:
         result = np.where(result >= threshold, 1, 0)
-    
+
     return result
+
 
 # Subtraction of two hypervectors
 def hv_sub(hv1, hv2):
@@ -118,6 +151,7 @@ def hv_sub(hv1, hv2):
     """
     return hv1 - hv2
 
+
 # Multiplication of two hypervectors
 def hv_mult(hv1, hv2):
     """
@@ -129,6 +163,7 @@ def hv_mult(hv1, hv2):
     np.ndarray: The resulting hypervector after multiplication.
     """
     return np.multiply(hv1, hv2)
+
 
 # Element-wise XOR of two hypervectors
 def hv_xor(hv1, hv2):
@@ -142,6 +177,7 @@ def hv_xor(hv1, hv2):
     """
     return np.bitwise_xor(hv1, hv2)
 
+
 # Multiplication of all hypervectors in a list
 def hv_mult_list(hvs):
     """
@@ -152,11 +188,12 @@ def hv_mult_list(hvs):
     np.ndarray: The resulting hypervector after multiplication.
     """
     result = hvs[0]
-    
+
     for hv in hvs[1:]:
         result = hv_mult(result, hv)
-    
+
     return result
+
 
 # XOR of all hypervectors in a list
 def hv_xor_list(hvs):
@@ -168,8 +205,8 @@ def hv_xor_list(hvs):
     np.ndarray: The resulting hypervector after XOR.
     """
     result = hvs[0]
-    
+
     for hv in hvs[1:]:
         result = hv_xor(result, hv)
-    
+
     return result
